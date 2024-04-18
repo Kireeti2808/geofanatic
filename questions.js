dbe052b9e1dfbd10565cc1d0afc09ffb536fe571
Kireeti2808 <157907809+Kireeti2808@users.noreply.github.com>
2024-04-18 20:21:16 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,61 @@
+let questions=[
+    {
+        numb:1,
+        question:"Which country has the highest number of Timezones?",
+        answer:"C.France",
+        options:[
+            "A.USA",
+            "B.Russia",
+            "C.France",
+            "D.UK"
+        ]
+    },
+    {
+        numb:2,
+        question:"What country has the most natural lakes?",
+        answer:"A.Canada",
+        options:[
+            "A.Canada",
+            "B.India",
+            "C.Finland",
+            "D.Philippines",
+
+        ]
+    },
+    {
+        numb:3,
+        question:"Which of the following are not the countries of UK?",
+        answer:"A.Ireland",
+        options:[
+            "A.Ireland",
+            "B.England",
+            "C.Wales",
+            "D.Scotland"
+
+        ]
+    },
+    {
+        numb:4,
+        question:"What is the name of the longest river in the US?",
+        answer:"B.Missouri River",
+        options:[
+            "A.Rio Grande",
+            "B.Missouri River",
+            "c.Mississippi River",
+            "D.Colorado River"
+            
+        ]
+    },
+    {
+        numb:5,
+        question:"Which of the following country's traffic runs on the left?",
+        answer:"D.Japan",
+        options:[
+            "A.China",
+            "B.Russia",
+            "C.Morocco",
+            "D.Japan"
+            
+        ]
+    }
+];
